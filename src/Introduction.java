c09250ab1ca030aa79d95063fa0ead482ba4eb66
username <54479676+CormacKrum@users.noreply.github.com>
2020-08-29 19:56:13 +0530

    Initial Commit

---
+++ src/Introduction.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.lang.*;
+
+public class Introduction {
+    public static void main(String[] args) throws IOException{
+        try {
+            System.out.println("Hello World");
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
