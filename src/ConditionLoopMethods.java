a23b6f9479c5408dd16b0a95e0dc205aeba30f99
username <54479676+CormacKrum@users.noreply.github.com>
2020-09-01 13:54:47 +0530

    Initial Commit

--- src/ConditionLoopMethods.java
+++ src/ConditionLoopMethods.java
@@ -1,5 +1,13 @@
-public class ConditionLoopMethods {
-    public static void main(String[] args){
+import java.util.Scanner;
 
+public class ConditionLoopMethods {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        if(year%4==0){
+            System.out.println("Leap Year");
+        } else {
+            System.out.println("Ordinary Year");
+        }
     }
 }
