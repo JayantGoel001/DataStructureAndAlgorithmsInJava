6b18a9d7acf0ef1e51c76a642d2a5c1e0397a75e
username <54479676+CormacKrum@users.noreply.github.com>
2020-09-04 17:43:03 +0530

    Initial Commit

--- src/ArrayListIntro.java
+++ src/ArrayListIntro.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class ArrayListIntro {
     public static void main(String[] args) {
@@ -7,5 +8,13 @@ public class ArrayListIntro {
             arrayList.add(i*2+7*i*i);
         }
         System.out.println(arrayList);
+        Collections.sort(arrayList);
+        ArrayList arrayList1 = new ArrayList();
+        arrayList1.add(1);
+        arrayList1.add("HEllo");
+        arrayList1.add('a');
+        arrayList1.add(1.0056);
+        System.out.println(arrayList1);
+
     }
 }
