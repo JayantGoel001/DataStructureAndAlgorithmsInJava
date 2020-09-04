06a6df2e8373fbc08259da8ce0430ba449e65dd4
username <54479676+CormacKrum@users.noreply.github.com>
2020-09-04 13:33:54 +0530

    Initial Commit

--- src/Array.java
+++ src/Array.java
@@ -13,6 +13,9 @@ public class Array {
         }
         System.out.println(Arrays.toString(ar));
         System.out.println(linearSearch(ar,5));
+        Arrays.sort(ar);
+        System.out.println(binarySearch(ar,5));
+        System.out.println(maxElementIndex(ar));
     }
     public static int linearSearch(int[] ar, int key){
         for (int i=0;i<ar.length;i++){
@@ -22,5 +25,37 @@ public class Array {
         }
         return -1;
     }
-    
+
+    public static int binarySearch(int[] ar,int key){
+
+        int start = 0;
+        int end = ar.length -1;
+        while (start<=end){
+            int mid = (start+end)/2;
+            if(ar[mid]==key){
+                return mid;
+            }
+            else if(ar[mid]<key){
+                start = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+        return -1;
+    }
+
+    public static int maxElementIndex(int []ar){
+        int index=0;
+        int maxEle = ar[0];
+        for (int i=0;i<ar.length;i++){
+            if(ar[i]>maxEle){
+                maxEle  = ar[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+
+
 }
