fb8e03a177bf9bcf37d49325ec94cd9e41ed1642
username <54479676+CormacKrum@users.noreply.github.com>
2020-09-06 20:11:04 +0530

    Initial Commit

--- src/Recursion.java
+++ src/Recursion.java
@@ -3,6 +3,21 @@ public class Recursion {
         print(5);
         System.out.println();
         printInc(13);
+        System.out.println();
+        System.out.println(fact(5));
+        int[] nums= {2,9,4,8,2,0,1};
+        int key = 8;
+        System.out.println(find(nums,0,key));
+    }
+
+    private static int find(int[] nums,int i,int key) {
+        if (i==nums.length){
+            return -1;
+        }
+        if (nums[i]==key){
+            return i;
+        }
+        return find(nums,i+1,key);
     }
 
     private static void print(int i) {
@@ -16,5 +31,12 @@ public class Recursion {
             printInc(i + 1);
             System.out.println(i);
         }
+    }
+
+    public static int fact(int n){
+        if (n==0){
+            return 1;
+        }
+        return n*fact(n-1);
     }
 }
