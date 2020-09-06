006917c34b00b0cd9e8db5095ab80db2d72306e7
username <54479676+CormacKrum@users.noreply.github.com>
2020-09-06 20:42:37 +0530

    Initial Commit

---
+++ src/StringRecursion.java
@@ -0,0 +1,17 @@
+public class StringRecursion {
+    public static void main(String[] args) {
+        String str = "abc";
+        subsec("",str);
+    }
+
+    private static void subsec(String proc, String str) {
+        if (str.isEmpty()){
+            System.out.println(proc);
+            return;
+        }
+        char ch = str.charAt(0);
+        subsec(proc,str.substring(1));
+        subsec(proc+ch,str.substring(1));
+
+    }
+}
