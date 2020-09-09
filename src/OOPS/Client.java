14efa787b5355c5f071e0e3a58973dd505312dfe
username <54479676+CormacKrum@users.noreply.github.com>
2020-09-09 20:40:16 +0530

    Initial Commit

---
+++ src/OOPS/Client.java
@@ -0,0 +1,10 @@
+package OOPS;
+
+public class Client {
+    public static void main(String[] args) {
+        Human ck = new Human();
+        System.out.println(ck.hands);
+        ck.eat();
+
+    }
+}
---
+++ src/OOPS/Human.java
@@ -0,0 +1,10 @@
+package OOPS;
+
+public class Human {
+    int hands = 4;
+    int legs = 2;
+    String name = "Anonymous";
+    public void eat(){
+        System.out.println(name+" eats Yippeee");
+    }
+}
