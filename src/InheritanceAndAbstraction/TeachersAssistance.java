791466d9e44bb2299081954dcf6625b399e97614
username <54479676+CormacKrum@users.noreply.github.com>
2020-09-12 20:41:39 +0530

    Initial Commit

--- src/InheritanceAndAbstraction/TeachersAssistance.java
+++ src/InheritanceAndAbstraction/TeachersAssistance.java
@@ -3,16 +3,16 @@ package InheritanceAndAbstraction;
 public class TeachersAssistance implements Student,Teacher{
     @Override
     public void play() {
-
+        System.out.println("Playing");
     }
 
     @Override
     public void study() {
-
+        System.out.println("Studying");
     }
 
     @Override
     public void teach() {
-
+        System.out.println("Teaching");
     }
 }
