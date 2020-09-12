6e683abd5e317f7235ac3743c4155c9a66768724
username <54479676+CormacKrum@users.noreply.github.com>
2020-09-12 19:17:12 +0530

    Initial Commit

---
+++ src/Inheritance/Car.java
@@ -0,0 +1,9 @@
+package Inheritance;
+
+public class Car extends Vehicle{
+    public static void main(String[] args) {
+        System.out.println(wheels);
+        start();
+    }
+    public int gate;
+}
---
+++ src/Inheritance/Vehicle.java
@@ -0,0 +1,9 @@
+package Inheritance;
+
+public class Vehicle {
+    public static int wheels;
+
+    public static void start(){
+        System.out.println("Start Like general vehicle.");
+    }
+}
